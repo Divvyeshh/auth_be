92c545821a12d2adf169e17086b967f27ef3cf74
Divvyeshh <divyesh.shivakumar2021@vitstudent.ac.in>
2023-09-24 13:42:58 +0530

    hero update

--- Desktop/Code/aathi/src/App.js
+++ Desktop/Code/aathi/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './component/navbar/Navbar';
+import Hero from './component/hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar className="nav"/>
+      <Hero className="hero"/>
     </div>
   );
 }
